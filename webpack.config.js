fb31108a1017938df78ba1181ae4b164d1f1551c
Jon Beasley <jonbeas@gmail.com>
2021-01-05 16:48:11 +0000

    setup build small changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path')
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'public', 'dist'),
         filename: 'bundle.js'
     },
     module: {
@@ -27,7 +27,9 @@ module.exports = {
     },
     devtool: "cheap-module-source-map",
     devServer: {
-        contentBase: path.join(__dirname, 'public')
+        contentBase: path.join(__dirname, 'public'),
+        open: true,
+        publicPath: '/dist/'
     }
 }
 
